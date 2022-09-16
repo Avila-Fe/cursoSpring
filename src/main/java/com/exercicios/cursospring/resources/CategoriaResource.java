39f60c76ca1659f61fe8289dd658622a7d0cf865
Felipe Avila <whiteavila6@gmail.com>
2022-09-16 13:23:29 -0300

    Rest testado

---
+++ src/main/java/com/exercicios/cursospring/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.exercicios.cursospring.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String teste(){
+        return "O rest está funcionando!";
+    }
+}
