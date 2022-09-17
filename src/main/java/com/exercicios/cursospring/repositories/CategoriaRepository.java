600afe991c7cd09366ebd8a580aa758c6d2e70ed
Felipe Avila <whiteavila6@gmail.com>
2022-09-17 16:21:40 -0300

    camadas

---
+++ src/main/java/com/exercicios/cursospring/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.exercicios.cursospring.repositories;
+
+import com.exercicios.cursospring.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/exercicios/cursospring/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.exercicios.cursospring.services;
+
+import com.exercicios.cursospring.domain.Categoria;
+import com.exercicios.cursospring.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
